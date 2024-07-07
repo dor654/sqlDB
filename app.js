43e2e6a92b95811a042877b67ee20d38f5929737
dor654 <daniyal.raja13@outlook.com>
2024-07-07 20:48:02 +0500

    new commit

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-// app.js
-const dotenv = require("dotenv");
-dotenv.config();
-
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const allowedOrigins = require("./config/allowedOrigins");
 
 const app = express();
+
+// Middleware to handle CORS
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectDB();
@@ -15,7 +27,7 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5050;
+const PORT = 5050;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
